06751c63ed7ac23beae9d23bc11c35eb221a4220
Adam Brown <ab185311@ncr.com>
2021-01-06 15:04:52 +0000

    call the GCP App Engine deployed service

--- src/api.js
+++ src/api.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const api = {
     getConfiguration: () =>
-        axios('http://localhost:8080/api/menu', {
-            method: 'GET',
-            headers: {
-                'x-tenant-id': 'test',
-            },
-        }),
+        axios(
+            'https://snowflake-menu-service-dot-dashboard-menu-service.ew.r.appspot.com/api/menu',
+            {
+                method: 'GET',
+                headers: {
+                    'x-tenant-id': 'test',
+                },
+            }
+        ),
 };
 export default api;
