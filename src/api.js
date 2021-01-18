860efc0769ab65c34e92e795221a477b6ab3ef45
Adam Brown <ab185311@ncr.com>
2021-01-18 14:12:47 +0000

    adding proxy config

--- src/api.js
+++ src/api.js
@@ -2,15 +2,12 @@ import axios from 'axios';
 
 const api = {
     getConfiguration: () =>
-        axios(
-            'https://snowflake-menu-service-dot-dashboard-menu-service.ew.r.appspot.com/api/menu',
-            {
-                method: 'GET',
-                headers: {
-                    'x-tenant-id': 'test',
-                },
-            }
-        ),
+        axios('/api/menu', {
+            method: 'GET',
+            headers: {
+                'x-tenant-id': 'test',
+            },
+        }),
     getContent: paras =>
         axios(`https://hipsum.co/api/?type=hipster-centric&paras=${paras}`),
 };
