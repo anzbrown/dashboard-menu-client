44414c796c77acb64297b0463a1116539a6dd5f5
Adam Brown <ab185311@ncr.com>
2021-01-18 13:32:18 +0000

    adding cloud build parameters for app engine deploy

--- src/api.js
+++ src/api.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 const api = {
     getConfiguration: () =>
-        axios('/api/menu', {
-            method: 'GET',
-            headers: {
-                'x-tenant-id': 'test',
-            },
-        }),
+        axios(
+            'https://snowflake-menu-service-dot-dashboard-menu-service.ew.r.appspot.com/api/menu',
+            {
+                method: 'GET',
+                headers: {
+                    'x-tenant-id': 'test',
+                },
+            }
+        ),
     getContent: paras =>
         axios(`https://hipsum.co/api/?type=hipster-centric&paras=${paras}`),
 };
