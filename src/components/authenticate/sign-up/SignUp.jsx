17ccc2ebffbf0146e3174a3ac63c30343a799bbd
Adam Brown <ab185311@ncr.com>
2021-01-22 16:39:16 +0000

    route signup path to dash

--- src/components/authenticate/sign-up/SignUp.jsx
+++ src/components/authenticate/sign-up/SignUp.jsx
@@ -21,7 +21,7 @@ export default function SignUp() {
             setError('');
             setLoading(true);
             await signup(emailRef.current.value, passwordRef.current.value);
-            history.push('/web/');
+            history.push('/');
         } catch {
             setError('Failed to create an account');
         }
